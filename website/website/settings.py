ff161ce04fcc7e5284a1c7447a676e0696d57ea7
Benjamin Drighès <tiresias.enst.fr>
2013-10-21 17:56:07 +0200

    Modified settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/pesto/test-webmining2013/website/static/'
+STATIC_ROOT = '/home/pesto/webmining2013/website/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -109,7 +109,7 @@ ROOT_URLCONF = 'website.urls'
 WSGI_APPLICATION = 'website.wsgi.application'
 
 TEMPLATE_DIRS = (
-  "/home/pesto/test-webmining2013/website/templates/cinema"
+  "/home/pesto/webmining2013/website/templates/cinema"
 )
 
 INSTALLED_APPS = (
