b2e424d458ceee5eeda36ac9291ee53e79340e28
David Tortel <tortel@c124-04.enst.fr>
2013-10-21 17:45:21 +0200

    IMDB Extractor

---
+++ crawling/Extractor/extractorHTML.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+# -*- coding: latin-1 -*-
+
+########################
+
+#importe les modules internes
+import Logger.init_logger as initLogger #Initialise le logger
+from Extractor.superExtractor import SuperExtractor #Charge la super classe
+
+import Logger.logger_config as loggerConfig
+import Extractor.extractor_config as extractorConfig
+
+#Importe les modules exterieures à l'application
+from lxml import etree
+from lxml.html.clean import Cleaner
+import StringIO
+
+logger = initLogger.getLogger(extractorConfig.EXTRACTOR_HTML_LOGGER_NAME)
+#########################
+
+
+
+
+""" La classe ExtractorHTML herite de la classe SuperExtractor. Elle permet de définir les fonctions nécessaire à l'extraction de données dans un document HTML, dont la chaine de caractère est donnée en paramètre de l'objet"""
+
+class ExtractorHTML(SuperExtractor):
+   def __init__(self,htmlString,cleaner):
+      SuperExtractor.__init__(self,htmlString)
+      self.htmlString = self.string.replace("\n","").replace("\r","")
+
+      self.parser = etree.HTMLParser()
+      self.cleaner = cleaner
+      self.cleanString = self.cleaner.clean_html(self.htmlString)
+
+      #Définit l'arbre sur lequel se feront toutes les XPath extraction
+      self.tree = etree.parse(StringIO.StringIO(self.cleanString),self.parser)
+      
+
+   def extractXpathElement(self,xpath,caller=None):
+      #Retourne un tableau d'éléments extraits
+      try :
+         e = self.tree.xpath(xpath)
+         res = [x.strip() for x in e if len(x.strip())>0]
+         if ((len(res) > 0) and (caller == None)) :
+            logger.debug(res)
+         elif (caller == None):
+            logger.warning("L'extraction de l'élément {} est de taille nulle".format(xpath))
+         return res
+
+      except : 
+         logger.error("Erreur dans l'extraction de l'élément {}".format(xpath))
+         return []
+
+   def extractXpathText(self,xpath):
+      s = self.extractXpathElement(xpath+'/text()')
+      return [x.strip() for x in s if len(x.strip())>0] 
+         
+   def extractTitle(self):
+      return self.extractXpathText('//title')
+
+   def extractH1(self):
+      return self.extractXpathText('//h1')
+
+   def extractH2(self):
+      return self.extractXpathText('//h2')
+
+   def extractH3(self):
+      return self.extractXpathText('//h3')
+
+   def extractStrong(self):
+      return self.extractXpathText('//strong')
+
+   def extractLink(self):
+      return self.extractXpathElement('//a/@href') #retourne un tableau de lien, certains absolus, certains relatifs
+
+   def extractLinkContent(self):
+      return self.extractXpathText('//a')
+
+   def extractEm(self):
+      return self.extractXpathText('//em')
+   
+   def extractImgAlt(self):
+      return self.extractXpathElement('//img/@alt')
+
+   def extractFooters(self):
+      pass
+
+   def extractMetaDescription(self):
+      return self.extractXpathElement('//meta/@name="description"')
+
+
---
+++ crawling/initScraper.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: latin-1 -*-
+
+
+""" The init.py script aims at checking out that the environment has been setting up correctly so that the application can be launched"""
+
+########################
+
+#importe les modules internes
+import Logger.init_logger as initLogger
+
+import Init.init_config as initConfig
+import Logger.logger_config as loggerConfig
+
+#Importe les modules exterieures à l'application
+import argparse
+
+#########################
+
+
+## Options Ligne de commande
+parser = argparse.ArgumentParser()
+
+#Liste tous les arguments que l'on peut saisir sur la ligne de commandes
+parser.add_argument('-fd', '--fresh_debug', dest = 'fresh_debug', help = "Si présent, le fichier de debug est vidé avant l'exécution", action='store_true')
+
+#Crée le tableau global qui donne accès aux arguments passés en paramètres sur la ligne de commande
+initConfig.args = parser.parse_args()
+
+##########
+# Crée les loggers & co
+logger = initLogger.getLogger (initConfig.SCRAPER_INIT_LOGGER_NAME)
+logger.debug('Logger {} créé'.format(initConfig.SCRAPER_INIT_LOGGER_NAME))
+
+#################
+# Vide le fichier de log si demandé
+debug_file = initConfig.RUN_TIME_FOLDER + loggerConfig.LOG_FILE
+if initConfig.args.fresh_debug:
+    logger.info ('Vide le fichier {}...'.format(debug_file))
+    open(debug_file, 'w').close()
+
+###############
+import IMDBExtractor.IMDBExtractor
