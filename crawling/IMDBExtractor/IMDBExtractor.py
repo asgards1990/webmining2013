f3b06d8ab8f3b88c121c5585354662f6f75790b7
David Tortel <tortel@c124-19.enst.fr>
2013-10-22 17:03:10 +0200

    IMDBExtractor construit des objets pour la DB

--- crawling/IMDBExtractor/IMDBExtractor.py
+++ crawling/IMDBExtractor/IMDBExtractor.py
@@ -19,6 +19,8 @@ import urllib
 
 import Extractor.customisedCleaner as CustomCleaner
 
+import re
+
 logger = initLogger.getLogger(IMDBExtractorConfig.IMDB_EXTRACTOR_LOGGER_NAME)
 
 ###################################################################
@@ -30,30 +32,72 @@ keywords_suffixe = '/keywords'
 film_page_default = '/title/'
 actor_page_default= '/name/'
 page_prefixe = 'http://www.imdb.com'
-film_id = 'tt1454468'
+film_id_ = 'tt1675434'
 the_artist_id="tt1655442"
 
 #################################################################
 
 
 class IMDBExtractor:
-
-   def __init__(self,url):
-      
+   #TODO voir si on peut actoriser en mettant dans un loader universel
+   def __init__(self,id_):
       logger.debug("Création de IMDB Extracteur")
+      self.id_=id_
+
+   def loadPage(self,url):
+      #TODO : vérifie si la page est déjà en local, sinon charge l'url
+      logger.info("La page n'existe pas en locale, chargement de la page {}".format(url))
       page = urllib.urlopen(url)
       t = page.read()
+      return t
+
+   def createExtractorEngine(self,url):
+      t=self.loadPage(url)
       cleaner = CustomCleaner.CustomedCleaner_HTML()
       self.extractor = ExtractorHTML(t,cleaner)
-      #logger.debug(self.extractor.cleanString)
-      logger.debug("IMDB Extracteur Créé pour la page {} ".format(url))
 
+class IMDBExtractor_Film(IMDBExtractor):
 
+   def __init__(self,film_id):
+      logger.debug("Création d'un Extracteur pour un type page de Film")
+      IMDBExtractor.__init__(self,film_id)
+
+      self.createExtractorEngine(page_prefixe+film_page_default+film_id)
+
+      self.english_title = self.extractTitle()
+      self.original_title = self.extractOriginalTitle()
+      self.release_date = self.extractReleaseDate()
+      self.runtime = self.extractRuntime()
+      self.budget = self.extractBudget()
+      self.box_office = self.extractBoxOffice()
+      self.imdb_user_rating = self.extractRatingValue()
+      self.imdb_nb_raters = self.extractRatingCount()
+      self.imdb_nb_user_review,self.imdb_nb_reviews = self.extractReviewCount() 
+      self.imdb_summary = self.extractSummary()
+      self.imdb_storyline = self.extractStoryLine()
+      #TODO metacritic_score = IntegerField(null=True,blank=True)
+      #TODO self.image_url = 
+      #TODO language = models.ForeignKey(Language, blank=True, null=True, on_delete=models.SET_NULL)
+      #TODO country = models.ManyToManyField(Country, related_name="films")
+      #TODO genres = models.ManyToManyField(Genre, related_name="films")
+      #TODO keywords = models.ManyToManyField(Keyword, related_name="films")
+      #TODO production_company = models.ManyToManyField(ProductionCompany, related_name="films")
+      self.directors = self.extractDirectors() #models.ManyToManyField(Person, related_name='films') #TODO : check with Django
+      #TODO writers = models.ManyToManyField(Person, related_name='films')
+      self.actors =  self.extractActors()
+      
    def extractTitle(self):
       logger.debug("Extract Title : ")
-      return self.extractor.extractXpathText('//td[@id="overview-top"]/h1[@class="header"]/span[@itemprop="name"]')
+      return self.extractor.extractXpathText('//td[@id="overview-top"]/h1[@class="header"]/span[@class="itemprop" and @itemprop="name"]')
+
+   def extractOriginalTitle(self):
+      #TODO : s'assurer du user agent
+      logger.debug("Extract Original Title : ")
+      a=self.extractor.extractXpathText('//td[@id="overview-top"]/h1[@class="header"]/span[@class="title-extra" and @itemprop="name"]') 
+      return self.english_title if not a else a
 
    def extractPoster(self):
+      #TODO extraire en local
       logger.debug("Extract Poster : ")
       return self.extractor.extractXpathElement('//td[@id="img_primary"]/*/a/img/@src')
 
@@ -65,34 +109,21 @@ class IMDBExtractor:
       logger.debug("Extract Release Date : ")
       return self.extractor.extractXpathElement('//td[@id="overview-top"]/div[@class="infobar"]/span[@class="nobr"]/a/meta[@itemprop="datePublished"]/@content')
 
-   def extractDuration(self):
-      logger.debug("Extract Duration : ")
+   def extractRuntime(self):
+      logger.debug("Extract Runtime : ")
       return self.extractor.extractXpathText('//td[@id="overview-top"]/div[@class="infobar"]/time[@itemprop="duration"]')
 
    def extractGenre(self):
       logger.debug("Extract Genre : ")
       return self.extractor.extractXpathText('//td[@id="overview-top"]/div[@class="infobar"]/a/span[@itemprop="genre"]')
 
-   def extractSynopsis(self):
+   def extractSummary(self):
       logger.debug("Extract Synopsis : ")
       return self.extractor.extractXpathText('//td[@id="overview-top"]/p[@itemprop="description"]')
 
    def extractStoryLine(self):
       logger.debug("Extract StoryLine  : ")
       return self.extractor.extractXpathText('//div[@class="inline canwrap" and @itemprop="description"]/p')
-
-   def extractKeywords(self):
-      logger.debug("Extract keywords : ")
-      return self.extractor.extractXpathText('//div[@class="see-more inline canwrap" and @itemprop="keywords"]/a/span')
-
-   def extractDirector(self):
-      logger.debug("Extract Directors : ")
-      #Main page
-      #return self.extractor.extractXpathText('//td[@id="overview-top"]/div[@itemprop="director"]/a/span[@itemprop="name"]')
-      #Page fullcredits
-      return self.extractor.extractXpathText('//td[@id="overview-top"]/div[@itemprop="director"]/a/span[@itemprop="name"]')
-
-      
 
    def extractWriter(self):
       logger.debug("Extract Writers : ")
@@ -102,13 +133,6 @@ class IMDBExtractor:
       logger.debug("Extract country : ")
       return self.extractor.extractXpathText('//div[@id="titleDetails"]/div/a[contains(@href,"country")]')
 
-   def extractProducers(self):
-      logger.debug("Extract Producers : ")
-      # Principal producers sur la main page
-      #return self.extractor.extractXpathText('//span[contains(@itemtype,"Organization") and @itemprop="creator"]/a/span[@itemprop="name"]')
-      return self.extractor.extractXpathText('//div[@class="article listo"]/div[@id="company_credits_content"]/ul[@class="simpleList"][1]/li/a')
-
-
    def extractBudget(self):
       logger.debug("Extract Budget : ")
       return self.extractor.extractXpathText('//h4[contains(text(),"Budget")]/parent::*')
@@ -116,6 +140,64 @@ class IMDBExtractor:
    def extractBoxOffice(self):
       logger.debug("Extract BoxOffice : ")
       return self.extractor.extractXpathText('//h4[contains(text(),"Gross")]/parent::*')
+
+   def extractRatingCount(self):
+      #Extrait le nombre de personne ayant voté sur le film
+      logger.debug("Extract rating count : ")      
+      return self.extractor.extractXpathText('//span[@itemprop="ratingCount"]')
+
+   def extractRatingValue(self):
+      #Extrait la note moyenne du film
+      logger.debug("Extract rating value : ")      
+      return self.extractor.extractXpathText('//span[@itemprop="ratingValue"]')
+
+   def extractReviewCount(self):
+      #Extrait le nombre de personne ayant commenté le film
+      logger.debug("Extract review count (user/critics) : ")      
+      return self.extractor.extractXpathText('//span[@itemprop="reviewCount"]')
+
+   def extractActors(self):
+      #TODO : charge la page des acteurs (1 en local, 2 avecurllib) // extrait les id des acteurs. Pour chaque acteurs on crée un objet IMDBExtract_Person
+      logger.debug("Extract Actors : ")
+      actor_url_list = self.extractor.extractXpathElement('//td[@itemprop="actor"]/a/@href')
+      return self.createPersonList(director_url_tab )
+
+   def createPersonList(self,p_url_list):
+      p_id_list = [re.match("/name/(nm[0-9]+)/", x).group(1) for x in p_url_list]
+      logger.debug(p_id_list)
+      person_list=[]
+      for person_id in p_id_list:
+         person_list.append(IMDBExtractor_Person(person_id))
+      return person_list
+
+   def extractDirectors(self):
+      #renvoie une liste de personne
+      logger.debug("Extract Directors : ")
+      director_url_tab = self.extractor.extractXpathElement('//td[@id="overview-top"]/div[@itemprop="director"]/a/@href')
+      return self.createPersonList(director_url_tab )
+
+   def extractProducers(self):
+      logger.debug("Extract Producers : ")
+      # Principal producers sur la main page
+      #return self.extractor.extractXpathText('//span[contains(@itemtype,"Organization") and @itemprop="creator"]/a/span[@itemprop="name"]')
+      #TODO : on charge la page du producteur (1- vérif en local; 2 : urllib) pour récupérer les info sur les producteurs
+      return self.extractor.extractXpathText('//div[@class="article listo"]/div[@id="company_credits_content"]/ul[@class="simpleList"][1]/li/a')
+
+
+class IMDBExtractor_Keywords(IMDBExtractor):
+   def __init__(self,url):
+      #TODO : on charge la page de keyword (1 en local 2 avec urllib) et on crée des objets Keywors
+      pass
+ 
+   def extractKeywords(self):
+      #TODO : on charge la page de keyword (1 en local 2 avec urllib) et on crée des objets Keywors
+      logger.debug("Extract Keywords : ")
+      return self.extractor.extractXpathText('//td/a')
+
+class IMDBExtractor_Awards(IMDBExtractor):
+   def __init__(self,url):
+      #TODO : on charge la page des awards  (1 en local 2 avec urllib) et on remplit les champs de Award 
+      pass
 
    def extractAwards(self):
       logger.debug("Extract Awards (à remettre en forme) : ")
@@ -133,94 +215,26 @@ class IMDBExtractor:
 	    award_category_status.pop(0)
             award_category_list.pop(0)
 
-   def extractActors(self):
-      logger.debug("Extract Actors : ")
-      return self.extractor.extractXpathText('//span[@itemprop="name"]')
+class IMDBExtractor_Person(IMDBExtractor):
 
-   def extractKeywords(self):
-      logger.debug("Extract Keywords : ")
-      return self.extractor.extractXpathText('//td/a')
+   def __init__(self,id_):
+      logger.debug("Création d'un Extracteur pour un type Person")
+      IMDBExtractor.__init__(self,id_)
+      self.createExtractorEngine(page_prefixe+actor_page_default+id_)
 
-   def extractRatingCount(self):
-      #Extrait le nombre de personne ayant voté sur le film
-      logger.debug("Extract rating count : ")      
-      return self.extractor.extractXpathText('//span[@itemprop="ratingCount"]')
-
-   def extractRatingValue(self):
-      #Extrait la note moyenne du film
-      logger.debug("Extract rating value : ")      
-      return self.extractor.extractXpathText('//span[@itemprop="ratingValue"]')
-
-   def extractReviewCount(self):
-      #Extrait le nombre de personne ayant commenté le film
-      logger.debug("Extract review count (user/critics) : ")      
-      return self.extractor.extractXpathText('//span[@itemprop="reviewCount"]')
+      self.birthDate = self.extractBirthDate()
+      self.birthCountry = self.extractBirthCountry()
 
    def extractBirthDate(self):
       #Extrait le nombre de personne ayant commenté le film
-      logger.debug("Extract Actor Birth Date : ")      
+      logger.debug("Extract Person Birth Date : ")      
       return self.extractor.extractXpathElement('//time[@itemprop="birthDate"]/@datetime')
 
    def extractBirthCountry(self):
       #Extrait le nombre de personne ayant commenté le film
-      logger.debug("Extract Actor Birth Date : ")      
+      logger.debug("Extract Person Birth Country : ")      
       return self.extractor.extractXpathText('//a[contains(@href,"?birth_place=")]')
 
-#IMDB PAGE
-
-def filmExtract(film_id_):
-
-   #MAIN
-   logger.debug('FILM MAIN INFO')
-   main_page = IMDBExtractor(page_prefixe+film_page_default+film_id_)
-
-   main_page.extractPoster()
-   main_page.extractTitle()
-   main_page.extractYear()
-   main_page.extractDuration()
-   main_page.extractGenre()
-   main_page.extractReleaseDate()
-   main_page.extractStoryLine()
-   main_page.extractCountry()
-   main_page.extractBudget()
-   main_page.extractBoxOffice()
-   main_page.extractDirector()
-   main_page.extractWriter()
-   main_page.extractRatingCount()
-   main_page.extractRatingValue()
-   main_page.extractReviewCount()
-
-   #CREDITS
-   logger.debug('FILM CREDIT')
-   company_credit_page = IMDBExtractor(page_prefixe+film_page_default+film_id_+companycredits_suffixe)
-   company_credit_page.extractProducers()
-
-   #IMBD AWARDS
-   logger.debug('FILM AWARS')
-   exAw = IMDBExtractor(page_prefixe+film_page_default+film_id_+awards_suffixe)
-   exAw.extractAwards()
-
-   #IMDB CAST
-   logger.debug('FILM CASTING')
-   cast_page = IMDBExtractor(page_prefixe+film_page_default+film_id_+cast_suffixe)
-   cast_page.extractActors()
-
-   #IMDB KEYWORDS
-   logger.debug('IMDB KEYWORDS')
-   keyword_page = IMDBExtractor(page_prefixe+film_page_default+film_id_+keywords_suffixe)
-   keyword_page.extractKeywords()
-
-   #IMDB CRITICS
-   
-
-def actorExtract(actor_id_):
-   logger.debug('ACTOR MAIN INFO')
-   main_page = IMDBExtractor(page_prefixe+actor_page_default+actor_id_)
-
-   main_page.extractBirthDate()
-   main_page.extractBirthCountry()
 
 
-filmExtract('tt1454468')
-#actorExtract('nm1082477')
-actorExtract('nm0000288')
+obj = IMDBExtractor_Film(film_id_)
