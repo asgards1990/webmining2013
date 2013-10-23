cccf63352c085b94b151e4a4597c99bcbdf9242c
Xavier Starkloff <tiresias.enst.fr>
2013-10-23 16:13:22 +0200

    See last commit

--- website/cinema/models.py
+++ website/cinema/models.py
@@ -8,7 +8,7 @@ class  Country (models.Model):
     nationality = models.CharField(max_length=255, blank=True)
     identifier = models.CharField(max_length=5, unique=True, default=None)
     def __unicode__(self):
-        return self.name       
+        return u"%s"%(self.name)       
     class Meta:
         ordering = ['name']
 
@@ -16,14 +16,14 @@ class Language(models.Model):
     name = models.CharField(max_length=255, unique=True, default=None)
     identifier = models.CharField(max_length=5, unique=True, default=None)
     def __unicode__(self):
-        return self.name    
+        return u"%s"%(self.name)   
     class Meta:
         ordering = ['name']
  
 class  Genre (models.Model):
     name = models.CharField(max_length=255, unique=True, default=None)
     def __unicode__(self):
-        return self.name
+        return u"%s"%(self.name)
     class Meta:
         ordering = ['name']
 
@@ -31,7 +31,7 @@ class  Reviewer (models.Model):
     name = models.CharField(max_length=255, unique=True, default=None)
     gender = models.CharField(max_length=1, validators=[RegexValidator('(m|f)')], null=True,blank=True) #later
     def __unicode__(self):
-        return self.name      
+        return u"%s"%(self.name)      
     class Meta:
         ordering = ['name']
 
@@ -40,7 +40,7 @@ class  Journal (models.Model):
     language = models.ForeignKey(Language, blank=True, null=True, on_delete=models.SET_NULL)
     country = models.ForeignKey(Country, blank=True, null=True, on_delete=models.SET_NULL) #later
     def __unicode__(self):
-        return self.name      
+        return u"%s"%(self.name)      
     class Meta:
         ordering = ['name']
 
@@ -54,7 +54,7 @@ class  Person (models.Model):
     first_name = models.CharField(max_length=255, blank=True) #later
     last_name = models.CharField(max_length=255, blank=True) #later
     def __unicode__(self):
-        return self.name
+        return u"%s"%(self.name)
     class Meta:
         ordering = ['name']
 
@@ -63,7 +63,7 @@ class  ProductionCompany (models.Model):
     name = models.CharField(max_length=255, unique=True, default=None)
     country = models.ForeignKey(Country, blank=True, null=True, on_delete=models.SET_NULL)
     def __unicode__(self):
-        return self.name      
+        return u"%s"%(self.name)      
     class Meta:
         ordering = ['name']
 
@@ -71,14 +71,14 @@ class  Institution (models.Model):
     name = models.CharField(max_length=255, unique=True, default=None)
     country = models.ForeignKey(Country, blank=True, null=True, on_delete=models.SET_NULL) #later
     def __unicode__(self):
-        return self.name      
+        return u"%s"%(self.name)      
     class Meta:
         ordering = ['name']
 
 class  Keyword (models.Model):
     word = models.CharField(max_length=255, unique=True, default=None)
     def __unicode__(self):
-        return self.word
+        return u"%s"%(self.word)
     class Meta:
         ordering = ['word']
 
@@ -110,7 +110,7 @@ class  Film (models.Model):
     writers = models.ManyToManyField(Person, blank=True, null=True, related_name='films_from_writer')
     actors = models.ManyToManyField(Person, blank=True, null=True, through='ActorWeight', related_name='films_from_actor')
     def __unicode__(self):
-        return self.original_title
+        return u"%s"%(self.original_title)
     class Meta:
         ordering = ['original_title', 'release_date']
 
@@ -120,7 +120,7 @@ class  Prize (models.Model):
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     def __unicode__(self):
-        return u"{0} in {1} for film {2} and Won {3}".format(self.institution.name, self.year, self.film.original_title, self.win)
+        return u"%s in %s for film %s. Won : %s"%(self.institution.name, self.year, self.film.original_title, self.win)
     class Meta:
         ordering = ['year']
 
@@ -133,7 +133,7 @@ class  Review (models.Model):
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     full_review_url = models.URLField(blank=True)
     def __unicode__(self):
-        return u"{0} from {1} for {2}".format(self.grade, self.reviewer.name, self.journal.name)       
+        return u"%s from %s for %s"%(self.grade, self.reviewer.name, self.journal.name)       
     class Meta:
         ordering = ['-grade']
 
@@ -143,14 +143,14 @@ class ActorWeight(models.Model):
     actor = models.ForeignKey(Person, on_delete=models.CASCADE)
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     def __unicode__(self):
-        return u"{0} played in {1} and is ranked {2} in credits and Star {3}".format(self.actor.name, self.film.original_title, self.rank, self.star)
+        return u"%s played in %s and is ranked %s in credits. Star : %s"%(self.actor.name, self.film.original_title, self.rank, self.star)
 
 class JournalInfluence(models.Model):
     influence = models.FloatField(null=True,blank=True)
     journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     def __unicode__(self):
-        return u"{0} has influence {1} for {2} films".format(self.journal.name, self.influence, self.genre.name)
+        return u"%s has influence %s for %s films"%(self.journal.name, self.influence, self.genre.name)
     class Meta:
         ordering = ['-influence']
 
@@ -160,7 +160,7 @@ class InstitutionInfluence(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     def __unicode__(self):
-        return u"{0} has influence {1} for {2} films in {3}".format(self.institution.name, self.influence, self.genre.name, self.country.name)
+        return u"%s has influence %s for %s films in %s"%(self.institution.name, self.influence, self.genre.name, self.country.name)
     class Meta:
         ordering = ['-influence']
 
@@ -169,6 +169,6 @@ class GenrePopularKeyword(models.Model):
     keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE)
     occurences = models.IntegerField()
     def __unicode__(self):
-        return u"{0} occurs {1} times in {2} films".format(self.keyword.word, self.occurences, self.genre.name)
+        return u"%s occurs %s times in %s films"%(self.keyword.word, self.occurences, self.genre.name)
     class Meta:
         ordering = ['-occurences']
