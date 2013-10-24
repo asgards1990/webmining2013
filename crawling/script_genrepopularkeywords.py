ba8c3fe3e9d11d2ba94f747f93092391d65db188
Xavier Starkloff <tiresias.enst.fr>
2013-10-24 15:06:47 +0200

    Correction du commit précédent

--- crawling/script_genrepopularkeywords.py
+++ crawling/script_genrepopularkeywords.py
@@ -1,15 +1,15 @@
 from cinema.models import *
 
 def addOccurence(genre, keyword):
-    result = GenrePopularKeywords.objects.filter(genre=genre, keyword=keyword)
+    result = GenrePopularKeyword.objects.filter(genre=genre, keyword=keyword)
     if result.exists():
         item = result[0]
         item.occurences = item.occurences + 1
         item.save()
     else:
-        Keyword.objects.create(genre=genre, keyword=keyword, occurences=1)
+        GenrePopularKeyword.objects.create(genre=genre, keyword=keyword, occurences=1)
 
-for film in films:
+for film in Film.objects.all():
     film_genres = film.genres.all()
     film_keywords = film.keywords.all()
     for film_genre in film_genres:
