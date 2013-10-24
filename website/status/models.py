72b4637a4aa3e8a4396007cc1f830bcefbb5563a
hollocou <alexandre@hollocou.fr>
2013-10-24 10:30:42 +0200

    Update status models and admin interface

--- website/status/models.py
+++ website/status/models.py
@@ -1,38 +1,44 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
 # Model for IMDB status
 class IMDBFilmStatus(models.Model):
-    imdb_id = models.CharField(max_length=9)            # the IMDB identifier
-    year = models.IntegerField()            # the release year
-    position = models.IntegerField()        # the page in which the film appears
-    # we need to download 5 webpages for each film
-    film_page = models.IntegerField()       
-    film_cast = models.IntegerField()
+    imdb_id = models.CharField(max_length=9)    # the IMDB identifier
+    year = models.IntegerField()                # the release year
+    position = models.IntegerField()            # the page in which the film appears
+    # we need to download several webpages for each film
+    film_mainpage = models.IntegerField()       
+    film_fullcredits = models.IntegerField()
     film_awards = models.IntegerField()
     film_reviews = models.IntegerField()
     film_keywords = models.IntegerField()
+    film_companycredits = models.IntegerField()
+    # film poster
+    film_image = models.IntegerField()
+    # status for downloader and extracter
+    downloaded = models.IntegerField()
     extracted = models.IntegerField()
     
-    def save(self, *args, **kwargs):
-        if len(self.imdb_id) != 9:
-            raise Exception, "invalid IMDB id (its length should be 9)"
-        elif self.year < 1950 or self.year > 2020:
-            raise Exception, "the year should be between 1950 and 2020"
-        elif self.position <= 0:
-            raise Exception, "the position should be > 0"
-        elif (self.film_page < 0 or self.film_page > 5) and self.film_page != 100:
-            raise Exception, "the 'film_page' field should be between 0 and 5, or equal to 100"
-        elif (self.film_cast < 0 or self.film_cast > 5) and self.film_cast != 100:
-            raise Exception, "the 'film_cast' field should be between 0 and 5, or equal to 100"
-        elif (self.film_awards < 0 or self.film_awards > 5) and self.film_awards != 100:
-            raise Exception, "the 'film_awards' field should be between 0 and 5, or equal to 100"
-        elif (self.film_reviews < 0 or self.film_reviews > 5) and self.film_reviews != 100:
-            raise Exception, "the 'film_reviews' field should be between 0 and 5, or equal to 100"
-        elif (self.film_keywords < 0 or self.film_keywords > 5) and self.film_keywords != 100:
-            raise Exception, "the 'film_keywords' field should be between 0 and 5, or equal to 100"
-        elif self.extracted < -1 or self.extracted > 1:
-            raise Exception, "the 'extracted' field should be -1, 0 or 1"
-        else:
-            super(IMDBstatus, self).save(*args, **kwargs) # Call the "real" save() method.
+    def __unicode__(self):
+        return u'%s status' % (self.imdb_id)
+    
+class IMDBPersonStatus(models.Model):
+    imdb_id = models.CharField(max_length=9)    # the IMDB identifier
+    # status for downloader and extracter
+    downloaded = models.IntegerField()
+    extracted = models.IntegerField()
+    
+    def __unicode__(self):
+        return u'%s status' % (self.imdb_id)
 
 
+class IMDBCompanyStatus(models.Model):
+    imdb_id = models.CharField(max_length=9)    # the IMDB identifier
+    # status for downloader and extracter
+    downloaded = models.IntegerField()
+    extracted = models.IntegerField()
+    
+    def __unicode__(self):
+        return u'%s status' % (self.imdb_id)
