4c57a73875576a8a30723433d209bc4f96f3db00
Xavier Starkloff <tiresias.enst.fr>
2013-10-28 09:13:48 +0100

    Rangement des scripts du dossier crawling

--- crawling/script_genres.py
+++
@@ -1,8 +0,0 @@
-from cinema.models import *
-
-# THIS SCRIPT FILLS ALL GENRE ENTRIES IN THE DATABASE
-
-genres_list = ['Action','Adventure','Animation','Biography','Comedy','Crime','Documentary','Drama','Family','Fantasy','Film-Noir','Game-Show','History','Horror','Music','Musical','Mystery','News','Reality-TV','Romance','Sci-Fi','Sport','Talk-Show','Thriller','War','Western']
-
-for item in genres_list:
-    Genre.objects.create(name=item)
