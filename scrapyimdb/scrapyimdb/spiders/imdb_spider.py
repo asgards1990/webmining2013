017f04dd4b4d229deb253c67d0d06fa75b934ea8
bdrighes <benjamin.drighes@gmail.com>
2013-10-28 09:01:21 +0100

    Renamed directories and created local spider for scrapyimdb.

--- scrapyimdb/scrapyimdb/spiders/imdb_spider.py
+++ scrapyimdb/scrapyimdb/spiders/imdb_spider.py
@@ -1,10 +1,9 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
-from scrapy.item import Item
 from scrapy.http import Request
 
-from imdb.items import *
+from scrapyimdb.items import *
 
 import re
 import dateutil.parser
