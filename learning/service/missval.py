65c64b2155086e596e32a9596b915cb848cb7106
Xavier Starkloff <xstarkloff@gmail.com>
2013-10-31 09:02:33 +0100

    Modifs fuck

--- learning/service/missval.py
+++ learning/service/missval.py
@@ -2,19 +2,13 @@ from cinema.models import Film
 from vectorizers import *
 import filmsfilter as flt
 import completer as cplt
+import regression as rgrs
+import classification as clsf
 import pylab as pl
 
 ### SCIKITLEARN ###########
 
-import sklearn
-
-#from sklearn.preprocessing import Imputer # manage missing values
-from sklearn.pipeline import Pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.cross_validation import cross_val_score
-
-############################
+# Filter films from database
     
 films = flt.filter1()
 
@@ -38,63 +32,39 @@ for name, fun in [('budget', getBudgetFeatures2),
     X.append(this_vec.toarray())
 
 y = getBoxOfficeFeatures2(films)
-y = y.ravel()
 
+# Data treatment
+
+y = y.ravel() # make it a vector
 X = np.concatenate(X,axis=1)
-
-# Imputer to complete missing values
-imputerX = Imputer(missing_values=-1)
-X[np.isnan(X)] = -1
-
-# X = imputer.fit_transform(X) : pour calculer les missing values (mieux en pipeline)
-# hist(log(y),20)
-
+X[np.isnan(X)] = -1 # replace NaN with -1 for missing values
+# hist(log(y),20) # print log(y) distribution
 y_log = np.log(y)
 
 ### CLASSIFICATION #######################################
-
-print clsf.getRandomForestClassifierScores(X,y_log)
-
 thresh = np.median(y_log)
-
 y_bin = y_log > thresh
-
-y_bin.mean()
-
-clf_class = RandomForestClassifier()
-
-pipeline_class = Pipeline([('imputer', imputer), ('clf', clf_class)])
-
-scores_class = cross_val_score(pipeline_class, X, y_bin, cv=3)
-
-print scores_class.mean(), 'chance level : %s' % y_bin.mean()
-
-###########################################################
+scores_class = clsf.getRandomForestClassifierCVScore(X,y_bin)
+print scores_class.mean()
 
 ### REGRESSION ###########################################
-
-clf = RandomForestRegressor()  #RandomForestClassifier()
-
-pipeline = Pipeline([('imputer', imputer), ('clf', clf)])
-
-scores = cross_val_score(pipeline, X, y_log, cv=3) # y_bin for Classification
-
+scores = rgrs.getRandomForestRegressorCVScore(X,y_log)
 print scores.mean(), 'chance level : %s' % y_bin.mean()
 
-yy = pipeline.fit(X, y_log).predict(X[1::2])
+#yy = pipeline.fit(X, y_log).predict(X[1::2])
 
 # pl.plot(y_log[1::2], yy, 'x')
 
 ## FEATURES IMPORTANCE
 
-clf.fit(X, y_log)
+#clf.fit(X, y_log)
 
 # pl.stem(range(X.shape[1]), clf.feature_importances_)
 
-fi = clf.feature_importances_
+#fi = clf.feature_importances_
 
-print (fi.argsort()[-5:]) # The 5 most important features
+#print (fi.argsort()[-5:]) # The 5 most important features
 
-imdb_nb_user_ratings=getImdbNbUserRatingsFeatures(filterFilms())
-pl.plot(imdb_nb_user_ratings,y_log,'+')
-pl.plot(np.log(imdb_nb_user_ratings),y_log,'+')
+#imdb_nb_user_ratings=getImdbNbUserRatingsFeatures(filterFilms())
+#pl.plot(imdb_nb_user_ratings,y_log,'+')
+#pl.plot(np.log(imdb_nb_user_ratings),y_log,'+')
---
+++ learning/service/regression.py
@@ -0,0 +1,11 @@
+import sklearn
+from sklearn.preprocessing import Imputer # manage missing values
+from sklearn.cross_validation import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+
+def getRandomForestRegressorCVScore(X,y)
+    imputer = Imputer(missing_values=-1)
+    clf_class = RandomForestClassifier()
+    pipeline_class = Pipeline([('imputer', imputer), ('clf', clf_class)])
+    return cross_val_score(pipeline_class, X, y, cv=3)
