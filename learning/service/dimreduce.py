2d19883242cfaf5e6c0060e54ccc7dea17c692c7
Xavier Starkloff <tiresias.enst.fr>
2013-11-01 07:56:04 +0100

    Création d'un fichier pour les fonctions de réduction de dimension des features

---
+++ learning/service/dimreduce.py
@@ -0,0 +1,16 @@
+#TODO by Xavier
+
+def reduceDimActors(X,n_clusters, print_clusters=False):
+	#returns X_reduced
+	return []
+
+def reduceDimDirectors(X_actors_reduced, films_iter):
+	#returns
+
+def actorsSpectralClustering(X,n_clusters):
+	#returns (vec,y) where vec is the dictvectorizer and y the labels
+	return []
+
+def printActorsClusters(vec,labels):
+	#prints the clusters for actors tuples
+	return []
