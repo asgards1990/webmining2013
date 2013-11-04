71ce47b363a1a61531f521cf9bfbe859556d9099
hollocou <alexandre@hollocou.fr>
2013-11-04 13:13:54 +0100

    Regression and classification tests

--- learning/service/filmsfilter.py
+++ learning/service/filmsfilter.py
@@ -1,4 +1,5 @@
 from cinema.models import Film
+from status.models import IMDBFilmStatus
 
 def filter1():
     print('Nb of films in DB : ' + str(Film.objects.count()))
@@ -19,6 +20,22 @@ def filter2(n): #like filter1 but returns n films, useful for tests
             film.imdb_nb_user_reviews=0
         if film.imdb_nb_reviews==None:
             film.imdb_nb_reviews=0
+    print('Nb of films after cleaning : ' + str(films.count()) + '. Selected ' + str(100*float(films.count())/Film.objects.count()) + ' %.')
+    return films
+
+
+def filter3(year): #like filter1 but returns n films, useful for tests
+    print('Nb of films in DB : ' + str(Film.objects.count()))
+    all_films = Film.objects.exclude(runtime=None).exclude(genres=None).exclude(country=None).exclude(imdb_user_rating=None).exclude(imdb_nb_user_ratings=None).exclude(box_office=None)
+    films = []
+    for film in all_films:
+        if film.imdb_nb_user_reviews==None:
+            film.imdb_nb_user_reviews=0
+        if film.imdb_nb_reviews==None:
+            film.imdb_nb_reviews=0
+        status = IMDBFilmStatus.objects.filter(imdb_id=film.imdb_id)
+        if len(status) > 0 and status[0].year >= year:
+            films.append(film)
     print('Nb of films after cleaning : ' + str(films.count()) + '. Selected ' + str(100*float(films.count())/Film.objects.count()) + ' %.')
     return films
 
