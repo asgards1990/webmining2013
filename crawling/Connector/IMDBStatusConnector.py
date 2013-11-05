79ef374220a73931c702f3883991974c7e7da056
David <chech@muse.(none)>
2013-11-05 21:15:24 +1100

    Filtered film extraction

--- crawling/Connector/IMDBStatusConnector.py
+++ crawling/Connector/IMDBStatusConnector.py
@@ -155,14 +155,13 @@ class IMDBFilmStatusConnector:
     def getDownloadedNotExtracted(self):
         self.logger.debug("Get the Film IMDB IDs in the database with downloaded=1 and extracted=0")
 
-        status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0, year__gte 1990, year__lte=2000, priority__lte=400).order_by("priority")
         status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0).order_by('priority')
         return map(lambda s: s.imdb_id, status)
 
-    def getDownloadedNotExtractedFiltered(self, year_min, priority_max):
+    def getDownloadedNotExtractedFiltered(self, year_min, year_max, priority_max):
         self.logger.debug("Get the Film IMDB IDs in the database with downloaded=1 and extracted=0 (year_min={0}, priority_max={1})".format(year_min, priority_max))
 
-        status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0, year__gte=year_min, priority__lte=priority_max).order_by('priority')
+        status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0, year__lte=year_max, year__gte=year_min, priority__lte=priority_max).order_by('priority')
         return map(lambda s: s.imdb_id, status)
 
     def getFilmPriority(self, imdb_id):
