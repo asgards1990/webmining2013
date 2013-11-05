215032918e3ba969699ab45d39d65dc52056e544
tortel <david.tortel@gmail.com>
2013-11-05 12:05:40 +0100

    minor changes

--- crawling/Connector/IMDBStatusConnector.py
+++ crawling/Connector/IMDBStatusConnector.py
@@ -155,11 +155,8 @@ class IMDBFilmStatusConnector:
     def getDownloadedNotExtracted(self):
         self.logger.debug("Get the Film IMDB IDs in the database with downloaded=1 and extracted=0")
 
-<<<<<<< HEAD
         status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0, year__gte 1990, year__lte=2000, priority__lte=400).order_by("priority")
-=======
         status = IMDBFilmStatus.objects.filter(downloaded=1, extracted=0).order_by('priority')
->>>>>>> 826223770fae3b48f589f5200318de2d394d4b75
         return map(lambda s: s.imdb_id, status)
 
     def getDownloadedNotExtractedFiltered(self, year_min, priority_max):
--- crawling/Logger/logger_config.py
+++ crawling/Logger/logger_config.py
@@ -3,9 +3,5 @@
 
 #Cette classe contient les paramètres de configuration pour le module Logger
 
-<<<<<<< HEAD
 LOG_FILE = 'debug8_films_1990_2000_400.log'
-=======
-LOG_FILE = 'debug_IMDB_COMPANY_DOWNLOADER_131104_002.log'
->>>>>>> 826223770fae3b48f589f5200318de2d394d4b75
 LOGGER_BASE_NAME = 'SUPER LOGGER'
