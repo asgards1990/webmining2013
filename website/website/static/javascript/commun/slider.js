7f35eb32204f203ea57f1bc2b14bb1709caf6887
jzbla <julien.szabla@gmail.Com>
2013-11-06 17:42:28 +0100

    Add Slider for Year in Explore filters

--- website/website/static/javascript/commun/slider.js
+++ website/website/static/javascript/commun/slider.js
@@ -15,3 +15,17 @@ $(function() {
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       "M - $" + $( "#slider-range" ).slider( "values", 1 )+"M" );
       });
+
+$(function() {
+    $( "#slider-rangeyear" ).slider({
+      range: true,
+      min: 1980,
+      max: 2013,
+      values: [ 1980, 2013 ],
+      slide: function( event, ui ) {
+        $( "#amountyear" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
+      }
+    });
+    $( "#amountyear" ).val($( "#slider-rangeyear" ).slider( "values", 0 ) +
+      " - " + $( "#slider-rangeyear" ).slider( "values", 1 ));
+      });
