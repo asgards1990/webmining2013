63b28c3da5ea3ad43e9d7c8d05920bc1ef37dcf8
jzbla <julien.szabla@gmail.Com>
2013-11-07 18:48:42 +0100

    Improve Bag of Words

--- website/website/static/javascript/prediction/bagofwords.js
+++ website/website/static/javascript/prediction/bagofwords.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
 	var fill = d3.scale.category20();
-	
-	d3.layout.cloud().size([300,300])
-	        .words([
-	            "Hello", "world", "normallyvrgtrbtnthn", "your", "want", "more", "words",
-	            "than", "this"].map(function (d) {
-	                    return {text: d, size: 10 + Math.random() * 40};
-	                }))
-	        .padding(5)
-	        .rotate(function () {
-	            return ~~(Math.random() * 2) * 90;
-	        })
-	        .font("Impact")
-	        .fontSize(function (d) {
-	            return d.size;
-	        })
-	        .on("end", draw)
-	        .start();
+    var jWord = ["abc","def","ghi"] ;
+    var jCount = [80, 50, 30];
+    var s = d3.scale.linear().range([0, 100]).domain([10,50]);
+    console.log(d3.zip(jWord, jCount));
+
+    d3.layout.cloud().size([420, 353])
+        /* .words([
+         "Hello", "world", "normallyvrgtrbtnthn", "your", "want", "more", "words",
+         "than", "this"].map(function (d) {
+         return {text: d, size: 10 + Math.random() * 40};
+         }))*/
+        .words(d3.zip(jWord, jCount).map(function (d) {
+            return {text: d[0], size: d[1]};
+        }))
+        .padding(5)
+                .rotate(function () {
+                    return ~~(Math.random() * 2) * 90;
+                })
+                .font("Impact")
+                .fontSize(function (d) {
+                    return d.size;
+                })
+                .on("end", draw)
+                .start();
 	
 	function draw(words) {
 	    d3.select("#bagofwords").append("svg")
-	            .attr("width", 400)
-	            .attr("height", 220)
+	            .attr("width", 420)
+	            .attr("height", 353)
 	            .append("g")
-	            .attr("transform", "translate(200,110)")
+	            .attr("transform", "translate(210,177)")
 	            .selectAll("text")
 	            .data(words)
 	            .enter().append("text")
