dda595205b5604cc8516b4650fba2d24de402cf2
hollocou <alexandre@hollocou.fr>
2013-11-07 14:59:32 +0100

    Prediction OK for box office and reviews

--- crawling/Logger/logger_config.py
+++ crawling/Logger/logger_config.py
@@ -3,5 +3,5 @@
 
 #Cette classe contient les paramètres de configuration pour le module Logger
 
-LOG_FILE = 'debug_1980_1990_800'
+LOG_FILE = 'debug_IMDB_PERSON_DOWNLOADER_131107_002'
 LOGGER_BASE_NAME = 'SUPER LOGGER'
