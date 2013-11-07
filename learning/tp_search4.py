a7abeb6efc75fca2c6a7ff8f8a441bfb980fcf1a
Xavier Starkloff <tiresias.enst.fr>
2013-11-07 19:49:24 +0100

    Debug des filtres du search

--- learning/tp_search4.py
+++ learning/tp_search4.py
@@ -7,14 +7,14 @@ criteria = {'actor_director':True,
           'budget':True,
           'review':True,
           'genre':True}
-filters = {'actors':[],
-	'directors':[],
+filters = {'actors':[], #DiCaprio nm0000138 Cotillard nm0182839
+	'directors':[], # James Cameron nm0000116 Scorcese nm0000217
 	'genres':[],#[Genre.objects.get(name='Action'), Genre.objects.get(name='Romance')],
-	'budget':{'min':5000000.,'max':10000000000000000000000.}, #TODO : warning, always use floats!
-	'reviews':{'min':0.}, #TODO : warning, always use floats!
-	'release_period':{'begin':'2010','end':'2020'}
+	'budget':{'min':0,'max':10000000000}, #TODO : warning, always use floats!
+	'reviews':{'min':0}, #TODO : warning, always use floats! beware 100
+	'release_period':{'begin':1901,'end':2020}
 	}
-filters=None
+
 args={'id':'tt0882969', # Avatar 'tt0499549' # Argo tt1024648' # 2012 'tt1190080' # Intouchables 'tt1675434'
 	'criteria':criteria,
 	'nbresults':10,
@@ -23,6 +23,6 @@ args={'id':'tt0882969', # Avatar 'tt0499549' # Argo tt1024648' # 2012 'tt1190080
 res = serv.search_request(args)
 
 for item in res['results']:
-    print item
+    print item['title']
 
 serv.quit()
