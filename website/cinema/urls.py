2115ff442543ad3c486fb76916d6743446dde3d6
Cédric HERMENT <cedric@macbook-pro-de-cedric-herment.local>
2013-11-07 11:17:01 +0100

    Cedric

--- website/cinema/urls.py
+++ website/cinema/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, url
 from views import formcall, resultsForm, predictionForm
 
-
-from django.conf.urls import patterns, include, url
-from django.shortcuts import render
-
 urlpatterns = patterns('cinema.views',
     url(r'^(.+)search/$', formcall),
     url(r'^results/$', resultsForm),
