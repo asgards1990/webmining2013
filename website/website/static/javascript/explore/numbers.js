1423b5a8fc649be055df04a68f440700997ddbd5
asgards1990 <ys19901010@hotmail.fr>
2013-11-08 10:11:14 +0100

    fin explore

--- website/website/static/javascript/explore/numbers.js
+++ website/website/static/javascript/explore/numbers.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#menu').mouseover(function() {
 	$('#actors > a > span').text($('#actors > ul > li > p > .checked').length);
+        $('#directors > a > span').text($('#directors > ul > li > p > .checked').length);
 	$('#genres > a > span').text($('#genres > ul > li > p > .checked').length);
     })
 });
