2dfca514099bb5089f1f5ab7e56a24d90a696806
Benjamin Drighès <tiresias.enst.fr>
2013-11-13 08:22:45 +0100

    Script on img.

--- crawling/scripts/script_fill_image_field.py
+++ crawling/scripts/script_fill_image_field.py
@@ -1,15 +1,9 @@
-from status.models import IMDBFilmStatus, IMDBPersonStatus
-from cinema.models import Film, Person
+from status.models import IMDBFilmStatus
+from cinema.models import Film
 
 films_with_img_downloaded = IMDBFilmStatus.objects.filter(film_image=1)
-persons_with_img_downloaded = IMDBPersonStatus.objects.filter(image=1)
 
 for s in films_with_img_downloaded:
     film = Film.objects.get(imdb_id = s.imdb_id)
     film.image_url = "poster/"+film.imdb_id+".jpg"
     film.save()
-
-for s in persons_with_img_downloaded:
-    person = Person.objects.get(imdb_id = s.imdb_id)
-    person.image_url = "picture/"+film.imdb_id+".jpg"
-    person.save()
