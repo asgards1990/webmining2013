8e9ba3c6c7c43aa257721866426cb9dc9bb18bb8
bdrighes <benjamin.drighes@gmail.com>
2013-11-17 12:32:47 +0100

    Modified filmInfo.

--- website/cinema/views.py
+++ website/cinema/views.py
@@ -8,39 +8,30 @@ def filmInfo(request):
     if request.method == 'POST':
         film_id = request.POST.get('film_id')
     else:
-        return HttpResponse("Erreur")
-
+        return HttpResponse("Field film_id required with POST method.")
+    
     try:
         film = Film.objects.get(imdb_id = film_id)
     except Film.DoesNotExist:
-        return HttpReponse("movie not found",)                
-
-    film = Film.objects.get(imdb_id = film_id)
+        return HttpReponse("No movie found for " + str(film_id) + ".")
+    
     actor_ids = ActorWeight.objects.filter(film=film).order_by('rank').values_list('actor_id')
     actors = Person.objects.in_bulk(actor_ids)
-    l = len(actors)
-    outputActors =[]
+    outputActors = []
+    
     for item in actor_ids[:5]:
-	actor=actors[item[0]]
-	actorDico = {'imdb_id':actor.imdb_id,'name':actor.name,'image_url':actor.image_url}  
-	outputActors.append(actorDico)
-	
-    genres = film.genres.all()
-    z = len(genres)
-    outputGenres =[]
-    for k in range(z):
-        genre = genres[k]
-        outputGenres.append(genre.name)
-
-
+	    actor=actors[item[0]]
+	    actorDico = {'imdb_id':actor.imdb_id,'name':actor.name,'image_url':actor.image_url}
+	    outputActors.append(actorDico)
+    
+    outputGenres = map(lambda e:e['name'], film.genres.values('name'))
+    
     directors = film.directors.all()
-    y = len(directors)
     outputDirectors =[]
-    for k in range(y):
-        director=directors[k]
-	directorDico = {'imdb_id':director.imdb_id,'name':director.name,'image_url':director.image_url}  
-	outputDirectors.append(directorDico)
-	
+    for director in directors:
+	    directorDico = {'imdb_id':director.imdb_id, 'name':director.name, 'image_url':director.image_url}
+	    outputDirectors.append(directorDico)
+    
     output = {'budget' : film.budget, 'plot': film.imdb_summary, 'poster':film.image_url, 'imbd_id': film.imdb_id,
               'english_title': film.english_title,'original_title':film.original_title, 'genres': outputGenres,
               'release_date':film.release_date.isoformat(),'actors':outputActors, 'directors':outputDirectors}
